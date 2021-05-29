a83ca5a0daf802892f98c57f64fb6e43c0d9a4de
akhil1610 <akhilb.1610@gmail.com>
2021-05-29 15:47:32 +0530

    first commit

---
+++ src/components/SidebarItem.js
@@ -0,0 +1,33 @@
+
+export const SidebarItem = [
+  {
+    title: 'Home',
+    path: '/',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Profile',
+    path: '/Profile',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Test',
+    path: '/Test',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Team',
+    path: '/team',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Messages',
+    path: '/messages',
+    cName: 'nav-text'
+  },
+  {
+    title: 'Support',
+    path: '/support',
+    cName: 'nav-text'
+  }
+];
